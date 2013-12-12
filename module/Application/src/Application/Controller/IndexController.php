a886dc36aa167972c777d3dc0a02a256bb2a1423
Jean <jadekler@gmail.com>
2013-12-11 22:38:55 -0500

    Finally got db connection working... wow. So annoyingly hard. Next up: services.

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -4,11 +4,35 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\Adapter\Driver\ResultInterface;
+use Zend\Db\ResultSet\ResultSet;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel(array("test" => "This is test data from the controller"));
+        $adapter = new Adapter(array(
+            'driver' => 'Mysqli',
+            'database' => 'jeansite',
+            'username' => 'root',
+            'password' => '',
+            'options' => array('buffer_results' => true)
+        ));
+
+        $sql = "SELECT * FROM jeansite.post";
+
+        $statement = $adapter->createStatement($sql);
+        $result = $statement->execute();
+
+        if ($result instanceof ResultInterface && $result->isQueryResult()) {
+            $resultSet = new ResultSet;
+            $resultSet->initialize($result);
+        }
+
+        return new ViewModel(array(
+            "test" => "This is test data from the controller",
+            "db" => $resultSet->toArray()
+        ));
     }
 }
