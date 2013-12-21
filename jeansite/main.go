8e3a23cd0197ded84cebec00832c181c1e1cd91f
Jean <jadekler@gmail.com>
2013-12-20 22:59:48 -0500

    Simplified everything

--- jeansite/main.go
+++ jeansite/main.go
@@ -1,90 +1,35 @@
 package main
 
 import (
-    "html/template"
+    //"html/template"
     "io/ioutil"
     "net/http"
-    "regexp"
+    //"regexp"
+    "os"
+    "fmt"
 )
 
-type Page struct {
-    Title string
-    Body []byte
-}
-
-func (p *Page) save() error {
-    filename := p.Title + ".txt"
-    return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
-    body, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return nil, err
-    }
-    return &Page{Title: title, Body: body}, nil
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request, title string) {
-    p, _ := loadPage(title)
-    renderTemplate(w, "index", p)
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-    p, err := loadPage(title)
-    if err != nil {
-        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-        return
-    }
-    renderTemplate(w, "view", p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-    p, err := loadPage(title)
-    if err != nil {
-        p = &Page{Title: title}
-    }
-    renderTemplate(w, "edit", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-    body := r.FormValue("body")
-    p := &Page{Title: title, Body: []byte(body)}
-    err := p.save()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-var templates = template.Must(template.ParseFiles("index.html", "edit.html", "view.html"))
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-    err := templates.ExecuteTemplate(w, tmpl+".html", p)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
-}
-
-var validPath = regexp.MustCompile("^/(index|edit|save|view)/([a-zA-Z0-9]*)$")
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        m := validPath.FindStringSubmatch(r.URL.Path)
-        if m == nil {
-            http.NotFound(w, r)
-            return
-        }
-        fn(w, r, m[2])
-    }
-}
-
 func main() {
-    http.HandleFunc("/index/", makeHandler(indexHandler))
-    http.HandleFunc("/view/", makeHandler(viewHandler))
-    http.HandleFunc("/edit/", makeHandler(editHandler))
-    http.HandleFunc("/save/", makeHandler(saveHandler))
-    http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/", indexPage)
+
+    fileServer := http.StripPrefix("/js/", http.FileServer(http.Dir("js")))
+    http.Handle("/js/", fileServer)
+
+    fileServer = http.StripPrefix("/html/", http.FileServer(http.Dir("html")))
+    http.Handle("/html/", fileServer)
+
+    err := http.ListenAndServe(":8080", nil)
+    checkError(err)
+}
+
+func indexPage(rw http.ResponseWriter, req *http.Request) {
+    index, _ := ioutil.ReadFile("html/index.html")
+    rw.Write([]byte(index))
+}
+
+func checkError(err error) {
+    if err != nil {
+        fmt.Println("Fatal error ", err.Error())
+        os.Exit(1)
+    }
 }
