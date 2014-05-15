442e54fdadf59a6c162eb2091c154eaf43627601
jadekler <jadekler@gmail.com>
2014-05-14 20:20:56 -0400

    Added bash profile, railstest2

---
+++ railstest/app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /users
+  # GET /users.json
+  def index
+    @test = "bam"
+  end
+end
