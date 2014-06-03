78a9fd567f8052a1927a2e8ab47d25435786fb14
Jean de Klerk <jadekler@gmail.com>
2014-06-02 20:04:16 -0600

    Working subclasses (is this correct syntax? explore more)

--- rails-todo/app/controllers/todo_controller.rb
+++ rails-todo/app/controllers/todo_controller.rb
@@ -2,6 +2,21 @@ class TodoController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @todos = ["bam", "boom"]
+    @todos = [Todo.new("todo 1", "some content"), Todo.new("todo 2", "some more content")]
+  end
+end
+
+class Todo
+  def initialize(title, content)
+    @Title = title
+    @Content = content
+  end
+
+  def Title
+    @Title
+  end
+
+  def Content
+    @Content
   end
 end
