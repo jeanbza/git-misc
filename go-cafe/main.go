f1e6f6212f381a42ec23b7f8a696e212af8ad54b
Jean de Klerk <jadekler@gmail.com>
2014-06-05 10:05:32 -0600

    Added some basic testing (working on this)

--- go-cafe/main.go
+++ go-cafe/main.go
@@ -12,5 +12,12 @@ func main() {
         }
 
         fmt.Printf("Read %v from stdin\n", input)
+        fmt.Printf("%v\n", orderCoffee(5))
     }
+}
+
+// Orders amountOfCoffees coffees and returns the price
+// when the coffees are ready
+func OrderCoffee(amountOfCoffees int) (price int) {
+    return amountOfCoffees*3
 }
---
+++ go-cafe/main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestOrderCoffee(t *testing.T) {
+    const in, out = 3, 15
+    if x := OrderCoffee(in); x != out {
+        t.Errorf("OrderCoffee(%v) = %v, want %v", in, x, out)
+    }
+}
