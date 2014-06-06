1ef5b50fc30e94e126d506336766dd7d6fdf06c9
Jean de Klerk <jadekler@gmail.com>
2014-06-05 19:33:06 -0600

    Fleshed out the order prompter

--- go-cafe/main.go
+++ go-cafe/main.go
@@ -1,19 +1,58 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "os"
+)
+
+type Orders struct {
+    coffees     int
+    teas        int
+    hotchocs    int
+}
 
 func main() {
-    var input = ""
+    orders := getOrders()
+    fmt.Printf("%v", orders)
+}
 
-    for input != "q" && input != "quit" {
+func getOrders() (Orders) {
+    var input = ""
+    orders := Orders{0,0,0}
+
+    fmt.Println("Welcome! What can I get you? We have coffee (c), hot chocolate (h), and tea (t). If you'd like to quit, press (q).")
+
+    for input != "d" && input != "done" {
         _, err := fmt.Scanf("%s", &input)
         if (err != nil) {
             
         }
 
-        fmt.Printf("Read %v from stdin\n", input)
-        fmt.Printf("%v\n", OrderCoffee(5))
+        switch input {
+            case "c", "coffee":
+                fmt.Println("One coffee, got it.")
+                orders.coffees++
+            case "h", "hot chocolate":
+                fmt.Println("One hot chocolate, got it.")
+                orders.hotchocs++
+            case "t", "tea":
+                fmt.Println("One tea, got it.")
+                orders.teas++
+            case "q", "quit":
+                os.Exit(1)
+            case "d", "done":
+            default:
+                fmt.Println("Huh?")
+        }
+
+        if input != "d" && input != "done" {
+            fmt.Println("What else can I get you?  We have coffee (c), hot chocolate (h), and tea (t). Or, are you done (d)?")
+        }
     }
+
+    fmt.Println("That's it? Ok!")
+
+    return orders
 }
 
 // Orders amountOfCoffees coffees and returns the price
