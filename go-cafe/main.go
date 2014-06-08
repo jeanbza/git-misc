2fd85c3a0cfd2659fbd08d3e1a7cf0df97998dbf
Jean de Klerk <jadekler@gmail.com>
2014-06-08 16:42:28 -0600

    Working goroutine examples

--- go-cafe/main.go
+++ go-cafe/main.go
@@ -9,38 +9,43 @@ import (
 
 func main() {
     costs := map[string]int {
-        "coffees": 5,
-        "teas": 4,
-        "hotchocs": 1,
+        "Coffee": 5,
+        "Tea": 4,
+        "Hot Chocolate": 1,
     }
 
     orders := getOrders()
-    fmt.Println("I'll let you know as soon as those orders are ready!")
+    fmt.Println("I'll let you know as soon as those orders are ready!\n")
     
     // Spawn goroutine for each order
-    // (later) spawn goroutines capped at max amount of barristas
-    completeOrders(orders)
+    // (todo) spawn goroutines capped at max amount of barristas
+    ch, totalOrders := completeOrders(orders)
 
-    // Give customer their order
+    for i := 0; i < totalOrders; i++ {
+        fmt.Println(<- ch)
+    }
+
+    // Tally total cost and give customer their order
     cost := tallyCost(orders, costs)
-    fmt.Printf("Here's your order! Your total comes to %d.", cost)
-
-    // Calculate total cost and give customer receipt
-    fmt.Println("Have a good day!")
-    var input string
-    fmt.Scanln(&input)
+    fmt.Printf("\nHere's your order! Your total comes to $%d. Have a good one!\n", cost)
 }
 
-func completeOrders(orders map[string]int) () {
+func completeOrders(orders map[string]int) (<- chan string, int) {
+    ch := make(chan string)
+    totalOrders := 0
+
     for product, amt := range orders {
         for i := 0; i < amt; i++ {
+            totalOrders++
             go func(product string) {
-                amt := time.Duration(rand.Intn(250))
+                amt := time.Duration(rand.Intn(1000))
                 time.Sleep(time.Millisecond * amt)
-                fmt.Printf("%v completed!", product)
+                ch <- fmt.Sprintf("%v completed!", product)
             }(product)
         }
     }
+
+    return ch, totalOrders
 }
 
 func tallyCost(orders map[string]int, costs map[string]int) (int) {
@@ -57,9 +62,9 @@ func getOrders() (map[string]int) {
     var input = ""
 
     orders := map[string]int{
-        "coffees": 0,
-        "teas": 0,
-        "hotchocs": 0,
+        "Coffee": 0,
+        "Tea": 0,
+        "Hot Chocolate": 0,
     }
 
     fmt.Println("Welcome! What can I get you? We have coffee (c), hot chocolate (h), and tea (t). If you'd like to quit, press (q).")
@@ -73,13 +78,13 @@ func getOrders() (map[string]int) {
         switch input {
             case "c", "coffee":
                 fmt.Println("One coffee, got it.")
-                orders["coffees"]++
+                orders["Coffee"]++
             case "h", "hot chocolate":
                 fmt.Println("One hot chocolate, got it.")
-                orders["hotchocs"]++
+                orders["Hot Chocolate"]++
             case "t", "tea":
                 fmt.Println("One tea, got it.")
-                orders["teas"]++
+                orders["Tea"]++
             case "q", "quit":
                 os.Exit(1)
             case "d", "done":
