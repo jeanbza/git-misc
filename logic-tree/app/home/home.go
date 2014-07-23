fe7fed09cba011b9103bb5f1c3d506a1bfbdee39
Jean de Klerk <jadekler@gmail.com>
2014-07-23 07:34:38 -0600

    Working sql gets

--- logic-tree/app/home/home.go
+++ logic-tree/app/home/home.go
@@ -9,26 +9,52 @@ import (
     "git-misc/logic-tree/app/common"
 )
 
+type Condition struct {
+    Field string
+    Operator string
+    Value int
+}
+
 func GetHomePage(rw http.ResponseWriter, req *http.Request) {
     type Page struct {
         Title string
+        Conditions []Condition
+    }
+
+    conditions := make([]Condition, 0)
+
+    rows, err := common.DB.Query("SELECT field, operator, value FROM logictree.conditions")
+    common.CheckError(err, 2)
+
+    var field, operator, value string
+    var valueInt int
+
+    for rows.Next() {
+        rows.Scan(&field, &operator, &value)
+        valueInt, err = strconv.Atoi(value)
+        common.CheckError(err, 2)
+
+        conditions = append(conditions, Condition{Field: field, Operator: operator, Value: valueInt})
     }
     
     p := Page{
         Title: "home",
+        Conditions: conditions,
     }
 
     common.Templates = template.Must(template.ParseFiles("templates/home/home.html", common.LayoutPath))
-    err := common.Templates.ExecuteTemplate(rw, "base", p)
+    err = common.Templates.ExecuteTemplate(rw, "base", p)
     common.CheckError(err, 2)
 }
 
 func SaveForm(rw http.ResponseWriter, req *http.Request) {
     field := req.FormValue("field")
     operator := req.FormValue("operator")
-    value, _ := strconv.Atoi(req.FormValue("value"))
+    value, err := strconv.Atoi(req.FormValue("value"))
+    common.CheckError(err, 2)
 
-    _, err := common.DB.Query(fmt.Sprintf("INSERT INTO logictree.conditions(field, operator, value) VALUES ('%s', '%s', %d)", field, operator, value))
+    _, err = common.DB.Query(fmt.Sprintf("INSERT INTO logictree.conditions(field, operator, value) VALUES ('%s', '%s', %d)", field, operator, value))
+    common.CheckError(err, 2)
 
     GetHomePage(rw, req)
 }
