f0a8895221107f492bbd85c4664024ffca1e95cc
Jean de Klerk <jadekler@gmail.com>
2014-08-03 22:11:05 -0600

    Added failing test for fullstack. I think unserialize is shitting up with pointers?

--- logic-tree/app/home/home_test.go
+++ logic-tree/app/home/home_test.go
@@ -9,6 +9,89 @@ func beforeEach() {
     fmt.Println("Starting test..")
 }
 
+// Fullstack test
+/**
+ * ((A && B) || C) && (D || E)
+ *              AND
+ *        OR           OR
+ *    AND     F      G    H
+ * A B C D E
+ */
+func TestFullstack(t *testing.T) {
+    beforeEach()
+
+    originalConditions := []Condition{
+        Condition{Text: "(", Type: "scope", Operator: "("},
+        Condition{Text: "(", Type: "scope", Operator: "("},
+        Condition{Text: "(", Type: "scope", Operator: "("},
+        Condition{Text: "age eq 4", Type: "equality", Field: "age", Operator: "eq", Value: "4"},
+        Condition{Text: "AND", Type: "logic", Operator: "AND"},
+        Condition{Text: "age eq 5", Type: "equality", Field: "age", Operator: "eq", Value: "5"},
+        Condition{Text: "AND", Type: "logic", Operator: "AND"},
+        Condition{Text: "age eq 6", Type: "equality", Field: "age", Operator: "eq", Value: "6"},
+        Condition{Text: "AND", Type: "logic", Operator: "AND"},
+        Condition{Text: "age eq 7", Type: "equality", Field: "age", Operator: "eq", Value: "7"},
+        Condition{Text: "AND", Type: "logic", Operator: "AND"},
+        Condition{Text: "age eq 8", Type: "equality", Field: "age", Operator: "eq", Value: "8"},
+        Condition{Text: ")", Type: "scope", Operator: ")"},
+        Condition{Text: "OR", Type: "logic", Operator: "OR"},
+        Condition{Text: "age eq 1", Type: "equality", Field: "age", Operator: "eq", Value: "1"},
+        Condition{Text: ")", Type: "scope", Operator: ")"},
+        Condition{Text: "AND", Type: "logic", Operator: "AND"},
+        Condition{Text: "(", Type: "scope", Operator: "("},
+        Condition{Text: "age eq 2", Type: "equality", Field: "age", Operator: "eq", Value: "2"},
+        Condition{Text: "OR", Type: "logic", Operator: "OR"},
+        Condition{Text: "age eq 3", Type: "equality", Field: "age", Operator: "eq", Value: "3"},
+        Condition{Text: ")", Type: "scope", Operator: ")"},
+        Condition{Text: ")", Type: "scope", Operator: ")"},
+    }
+
+    treeRootExpectation := &treeNode{Parent: nil, Children: nil, Node: Condition{Text: "AND", Type: "logic", Operator: "AND"}}
+
+    child1 := treeNode{Parent: nil, Children: nil, Node: Condition{Text: "OR", Type: "logic", Operator: "OR"}}
+    child2 := treeNode{Parent: nil, Children: nil, Node: Condition{Text: "OR", Type: "logic", Operator: "OR"}}
+    treeRootExpectation.Children = []*treeNode{&child1, &child2}
+
+    child3 := treeNode{Parent: &child1, Children: nil, Node: Condition{Text: "AND", Type: "logic", Operator: "AND"}}
+    child4 := treeNode{Parent: &child1, Children: nil, Node: Condition{Text: "age eq 1", Type: "equality", Field: "age", Operator: "eq", Value: "1"}}
+    child1.Children = []*treeNode{&child3, &child4}
+
+    child5 := treeNode{Parent: &child2, Children: nil, Node: Condition{Text: "age eq 2", Type: "equality", Field: "age", Operator: "eq", Value: "2"}}
+    child6 := treeNode{Parent: &child2, Children: nil, Node: Condition{Text: "age eq 3", Type: "equality", Field: "age", Operator: "eq", Value: "3"}}
+    child2.Children = []*treeNode{&child5, &child6}
+
+    child7 := treeNode{Parent: &child3, Children: nil, Node: Condition{Text: "age eq 4", Type: "equality", Field: "age", Operator: "eq", Value: "4"}}
+    child8 := treeNode{Parent: &child3, Children: nil, Node: Condition{Text: "age eq 5", Type: "equality", Field: "age", Operator: "eq", Value: "5"}}
+    child9 := treeNode{Parent: &child3, Children: nil, Node: Condition{Text: "age eq 6", Type: "equality", Field: "age", Operator: "eq", Value: "6"}}
+    child10 := treeNode{Parent: &child3, Children: nil, Node: Condition{Text: "age eq 7", Type: "equality", Field: "age", Operator: "eq", Value: "7"}}
+    child11 := treeNode{Parent: &child3, Children: nil, Node: Condition{Text: "age eq 8", Type: "equality", Field: "age", Operator: "eq", Value: "8"}}
+    child3.Children = []*treeNode{&child7, &child8, &child9, &child10, &child11}
+
+    // Unserialize into a tree
+    var expectedOutErr error
+
+    treeReturned, errorsReturned := unserializeTree(originalConditions)
+
+    if !treeReturned.matches(treeRootExpectation) {
+        t.Errorf("unserializeTree(%v) - got %v, want %v", originalConditions, treeReturned.print(), treeRootExpectation.print())
+    }
+
+    if errorsReturned != expectedOutErr {
+        t.Errorf("unserializeTree(%v) errorsReturned - got %v, want %v", originalConditions, errorsReturned, expectedOutErr)
+    }
+
+    // Serialize back into conditions array
+    conditionsReturned, errorsReturned := serializeTree(treeReturned)
+
+    if !matchesArray(conditionsReturned, originalConditions) {
+        t.Errorf("serializeTree(%v) conditionsReturned - got %v, want %v", treeReturned, simplifyConditions(conditionsReturned), simplifyConditions(originalConditions))
+    }
+
+    if errorsReturned != expectedOutErr {
+        t.Errorf("serializeTree(%v) errorsReturned - got %v, want %v", treeReturned, errorsReturned, expectedOutErr)
+    }
+}
+
 func TestParseJSON(t *testing.T) {
     beforeEach()
 
