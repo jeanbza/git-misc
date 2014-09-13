ccdf58a8352f7c6201a3f07fc12754ea394eec43
Jean de Klerk <jadekler@gmail.com>
2014-09-13 17:52:24 -0600

    Switched to using deferred promise model

--- js/animations.js
+++ js/animations.js
@@ -1,28 +1,24 @@
 var main = function() {
     var timeout = 200;
+    var allCirclesFinished = [];
 
-    for (k = 0; k < 10; k++) {
-        (function(i) {setTimeout(function() {
-            $(".circle-div").each(function(j) {
-                var self = $(this);
-                setTimeout(function() {moveUp(self, 0, i%2!=0);}, timeout*j);
-            });
-        }, $(".circle-div").length*timeout*i); } (k));
+    moveCircles(false);
+    
+    function moveCircles(up) {
+        $(".circle-div").each(function(j) {
+            var self = $(this);
+            var promiseToFinish = new $.Deferred();
+            allCirclesFinished.push(promiseToFinish);
+            setTimeout(function() {moveCircle(self, 0, up, promiseToFinish);}, timeout*j);
+        });
+
+        $.when.apply($, allCirclesFinished).then(function() {
+            allCirclesFinished = [];
+            moveCircles(!up);
+        });
     }
 
-    // $(".circle-div").each(function(j) {
-    //     var self = $(this);
-    //     setTimeout(function() {moveUp(self, 0, false);}, timeout*j);
-    // });
-
-    // setTimeout(function() {
-    //     $(".circle-div").each(function(j) {
-    //         var self = $(this);
-    //         setTimeout(function() {moveUp(self, 0, true);}, timeout*j);
-    //     });
-    // }, $(".circle-div").length*timeout);
-
-    function moveUp(elem, i, up) {
+    function moveCircle(elem, i, up, promise) {
         var marginTop = elem.css('margin-top').substring(0, elem.css('margin-top').length-2);
 
         if (up) {
@@ -34,7 +30,9 @@ var main = function() {
         i++;
 
         if (i < 15) {
-            setTimeout(function() {moveUp(elem, i, up);}, 50)
+            setTimeout(function() {moveCircle(elem, i, up, promise);}, 50)
+        } else {
+            promise.resolve();
         }
     }
 };
