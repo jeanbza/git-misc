a671aa691b4fc0e910c886ce61e66fb2b7928d03
Jean de Klerk <jadekler@gmail.com>
2014-09-27 09:04:28 -0600

    Initial commit for localization prototype

---
+++ localizer/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "io/ioutil"
+    "net/http"
+    "regexp"
+)
+
+func main() {
+    response, err := http.Get("http://localhost:8080")
+    handleErr(err)
+
+    bodyByte, err := ioutil.ReadAll(response.Body)
+    handleErr(err)
+
+    bodyStr := string(bodyByte)
+
+    fmt.Println("Before")
+    outputLinks(bodyStr)
+
+    bodyStrReplaced := replaceLinks(bodyStr)
+
+    fmt.Println("After")
+    outputLinks(bodyStrReplaced)
+}
+
+func replaceLinks(bodyStr string) string {
+    hrefRegex := regexp.MustCompile(`(href|src)="(.*)"`)
+    result := hrefRegex.ReplaceAllString(bodyStr, "$1=\"vagrant.$2\"")
+
+    return result
+}
+
+func outputLinks(bodyStr string) {
+    hrefRegex := regexp.MustCompile(`(href|src)=".*"`)
+    result := hrefRegex.FindAllString(bodyStr, -1)
+
+    for _, val := range result {
+        fmt.Println(val)
+    }
+}
+
+func handleErr(err error) {
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(0)
+    }
+}
