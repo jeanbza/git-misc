5ed45f0a7b0b9b7b3b51a5943a0422de7a6ad714
Jean de Klerk <jadekler@gmail.com>
2014-09-27 14:05:09 -0600

    Working circle. Now to clean up.

--- circles/circle-circle/js/animations.js
+++ circles/circle-circle/js/animations.js
@@ -1,17 +1,26 @@
-function addMargin(elem, marginToAdd) {
-    var marginTop = elem.css('margin-top').substring(0, elem.css('margin-top').length-2);
-    margin = parseInt(marginTop)+marginToAdd+'px';
-    return margin;
+function adjustPosition(elem, degree) {
+    var radius = 100;
+    var marginX = radius*Math.cos(degree);
+    var marginY = radius*Math.sin(degree);
+
+    console.dir(" ");
+    console.dir(degree);
+    console.dir(marginY);
+    
+    $(elem).css("margin-top", marginY);
+    $(elem).css("margin-left", marginX);
 }
 
+// (x0,y0) and whose radius is r is (x0 + r cos theta, y0 + r sin theta)
 var main = function() {
     var animationTime = 2;
 
     for (i = 0; i < 100; i++) {
-        $(".circle-div").delay(i*animationTime).animate({
-            'margin-top': addMargin($(".circle-div"), 3*i),
-            'margin-left': addMargin($(".circle-div"), 1*i)
-        }, animationTime);
+        (function(i) {
+            setTimeout((function() {
+                adjustPosition($(".circle-div"), i/10);
+            }), i*20);
+        })(i);
     }
 };
 
