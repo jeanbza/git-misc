e91b399769b0c87519993e9a75d0402c2b760c73
Jean de Klerk <jadekler@gmail.com>
2014-09-27 20:47:36 -0400

    Continuously rotating circle

--- circles/circle-circle/js/animations.js
+++ circles/circle-circle/js/animations.js
@@ -2,26 +2,31 @@ function adjustPosition(elem, degree) {
     var radius = 100;
     var marginX = radius*Math.cos(degree);
     var marginY = radius*Math.sin(degree);
-
-    console.dir(" ");
-    console.dir(degree);
-    console.dir(marginY);
     
     $(elem).css("margin-top", marginY);
     $(elem).css("margin-left", marginX);
 }
 
-// (x0,y0) and whose radius is r is (x0 + r cos theta, y0 + r sin theta)
-var main = function() {
+function circleOnce() {
+    var fullRotation = 314*2;
     var animationTime = 2;
 
-    for (i = 0; i < 100; i++) {
+    for (i = 0; i < fullRotation; i++) {
         (function(i) {
             setTimeout((function() {
-                adjustPosition($(".circle-div"), i/10);
-            }), i*20);
+                adjustPosition($(".circle-div"), i/100);
+            }), i*animationTime);
         })(i);
     }
+
+    setTimeout(function() {
+        circleOnce();
+    }, animationTime*fullRotation);
+}
+
+// (x0,y0) and whose radius is r is (x0 + r cos theta, y0 + r sin theta)
+var main = function() {
+    circleOnce();
 };
 
 $(document).ready(main);
